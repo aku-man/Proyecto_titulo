59d72071c14e9d745cb6581a8c63eef2f781cc0a
Franco Valdivia <fv0152@gmail.com>
2020-09-05 16:13:28 -0400

    Eliminado componente drag and drop

--- src/app/components/contenedor/contenedor.component.ts
+++ src/app/components/contenedor/contenedor.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, Input, EventEmitter } from '@angular/core';
-import { Aux } from '../../models/auxiliar.model';
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Pictograma } from 'src/app/models/pictograma.model';
 
 import {ListaPictogramas} from '../../ListaPictogramas';
@@ -10,30 +8,26 @@ import {ListaPictogramas} from '../../ListaPictogramas';
   templateUrl: './contenedor.component.html',
   styleUrls: ['./contenedor.component.css']
 })
-export class ContenedorComponent implements OnInit {
+export class ContenedorComponent implements OnInit, OnChanges {
   lista = ListaPictogramas;
   listaPict: Pictograma[] = [];
   @Input() catSelected: number;
-  @Input() pictoSelected :number;
+  @Input() pictoSelected: number;
 
   constructor() { }
+
+  ngOnChanges(): void{
+    for (const item of this.lista){
+      if (item.id === this.catSelected){
+        this.listaPict = item.pictogramas;
+      }
+    }
+  }
 
   ngOnInit(): void {
     console.log(this.catSelected);
     console.log(this.pictoSelected);
   }
-
- /*  drop(event: CdkDragDrop<string[]>): void{
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
-    }
-    console.log(this.catSelected);
-  } */
   
   
 }
--- src/app/components/selector-imagenes/selector-imagenes.component.ts
+++ src/app/components/selector-imagenes/selector-imagenes.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges, Output, EventEmitter} from '@angular/core';
-// import { Pictograma } from '../../models/pictograma.model';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import {ActivatedRoute} from '@angular/router';
 import {ListaPictogramas} from '../../ListaPictogramas';
 import { Aux } from '../../models/auxiliar.model';
@@ -18,7 +16,14 @@ export class SelectorImagenesComponent implements OnInit, OnChanges {
   lista: any;
   categoriaid: number;
   constructor(private route: ActivatedRoute) { }
+
   @Input() catSelected: number;
+  @Output() messageEvent = new EventEmitter<number>();
+
+  escogerPicto(id: number): void{
+    this.messageEvent.emit(id);
+    // console.log(id);
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     for (const item of this.listaAux) {
@@ -29,33 +34,7 @@ export class SelectorImagenesComponent implements OnInit, OnChanges {
   }
 
   ngOnInit(): void {
-    // this.route.paramMap.subscribe(params =>{
-    //   const categoria = Number(params.get('categoriaId'));
-    //   console.log(categoria);
-    //   this.lista = this.listaPicto.find(id => id.id === categoria);
-    //   this.categoriaid = categoria;
-    //   console.log(this.categoriaid);
-    //   console.log(this.lista);
-    //   });
-    this.escogerPicto(5);
-  }
-
-  /* drop(event: CdkDragDrop<string[]>): void{
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
-    }
-  } */
-
-  
-  @Output() messageEvent = new EventEmitter<number>();
-  escogerPicto(id :number):void{
-    this.messageEvent.emit(id);
-    //console.log(id);
+    // this.escogerPicto(5);
   }
 
 }
